7c4dab7b647d075265790c577730cb5a6a914530
evanjmg <evanjmg@gmail.com>
2015-08-20 11:01:13 +0100

    started exercism testing

---
+++ javascript/bob/bob.js
@@ -0,0 +1,23 @@
+//
+// This is only a SKELETON file for the "Bob" exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+var Bob = function() {};
+
+Bob.prototype.hey = function(input) {
+  if (input === '') {
+    return 'Fine. Be that way'
+  }
+
+// If input is all caps, tell 'Whoa, chill out!'
+// If weird and unrecognized - 'Whatever.' -else
+//  if statement contains a '?', say 'Sure.'
+// if all caps and 3 concecutive vowels
+// if numbers & all caps letters, whoa chillout
+// number with ? = 'Sure.'
+// if '' - 'Fine.Be that way'
+
+};
+
+module.exports = Bob;
